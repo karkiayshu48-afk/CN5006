63d8132b0dac1ee1a8d5fcfbc0ff54e14a931fe8
karkiayshu48-afk <karkiayshu48@gmail.com>
2025-10-22 10:54:56 +0000

    WEEK 4 LAB

--- WEEK 4/index.js
+++ WEEK 4/index.js
@@ -1,1 +1,22 @@
 
+var express=require("express")
+
+var fs= require("fs")
+
+var app= express()
+
+var bodyParser = require("body-parser");
+
+app.get('/',function(req,res){res.send("hello it is my first express application")})
+
+app.get('/about',function(req,res)
+
+{ res.send("This is basic express application ")
+})
+app.get('/users/:userId/books/:bookId', function (req, res) { res.send(req.params)
+})
+
+
+
+
+app.listen(5000,function(){console.log("server is running on port 5000")})
